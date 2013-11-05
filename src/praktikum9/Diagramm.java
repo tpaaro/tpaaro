857197c4f2c94f1e25e160efe868123b6d06cfd5
Tanel <Tanel@EDI>
2013-11-05 19:21:42 +0200

    zfag

--- src/praktikum9/Diagramm.java
+++ src/praktikum9/Diagramm.java
@@ -11,27 +11,23 @@ import lib.TextIO;
 //mitte lubada. Trükkida ekraanile x tähtedest koosnev tulpdiagramm, 
 //kus x-e on täpselt sama palju kui oli numbreid.
 
-public class TulpDiagramm {
-	
-	
- 
+public class Diagramm {
+
 	public static void main(String[] args) {
 		List<Integer> tulbad = new ArrayList<>();
-		TextIO.putln("Sisesta (ainult positiivseid) numbreid et koostada tulbad.");
-		do{
-	    int x = TextIO.getlnInt();
-		tulbad.add(x);
-		System.out.println(tulbad);
-	
-		}while(TextIO.getlnInt() != 0);
-		
-		
-
-		}
+		int tulpadeArv;
+		do {
+			tulpadeArv = TextIO.getlnInt();
+			if (tulpadeArv > 0)
+				tulbad.add(tulpadeArv);
+		} while (tulpadeArv != 0);
+for(int item:tulbad){
+	System.out.print(item+ " " );
+	String result=""; 
+	for(int i = 0; i<Math.min(item,80); i++)
+	result += "*";
+	System.out.println(result);
+}
+	}
 
 }
-
-
-	
-	
-	
