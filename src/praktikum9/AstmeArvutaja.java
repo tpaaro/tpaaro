60fe4e666d10c17c6aa1afa403b1167df02c9853
Tanel <Tanel@EDI>
2013-11-05 19:48:56 +0200

    uh

--- src/praktikum9/AstmeArvutaja.java
+++ src/praktikum9/AstmeArvutaja.java
@@ -3,15 +3,24 @@ package praktikum9;
 public class AstmeArvutaja {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(astenda(2, 3));
 
 	}
+
 	public static int astenda(int arv, int aste) {
-		int i= 5;
-		String x = "x";
-		for(int t = 0; t<=i; t++){
-		
+		int x;
+		if (aste == 0) {
+			x = 1;
+			return x;
+		} else if (aste == 1) {
+			x = arv;
+			return x;
+		} else {
+
+			x = arv * astenda(arv, aste - 1);
+			return x;
 		}
-	    
+
 	}
+
 }
