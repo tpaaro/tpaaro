94ccb547d5209dfdc4fa7880d90928a3d26e70d2
Tanel <Tanel@EDI.lan>
2013-11-09 15:25:53 +0200

    iho

--- src/praktikum10/Fibonacci.java
+++ src/praktikum10/Fibonacci.java
@@ -4,18 +4,27 @@ import lib.TextIO;
 
 public class Fibonacci {
 
-	public static int fib(int n) {
-		if (n <= 1) return n;
-		else return fib (n-1) + fib(n-2);
-		 }
-	
-	public static void main(String[] args) {
-		while(true){
-		System.out.println("Mitmendat Fibonacci arvu soovite teada? Sisestage jrk number.");
-			int i = TextIO.getlnInt()-1;
-			 System.out.println(i + 1 + ". Fibonacci arv on: " + fib(i));
+	public static long fib(int n) {
+		if (n <= 1)
+			return n;
+		else
+			return fib(n - 1) + fib(n - 2);
 		}
+	
+
+	public static void main(String[] args) {
+		
+		    int n = 100;
+			for( int t = 0; t < n; t++){
+				//System.out
+					//.println("Mitmendat Fibonacci arvu soovite teada? Sisestage jrk number.");
+		
+			//int i = TextIO.getlnInt() - 1;
+			System.out.println(t + 1 + ". Fibonacci arv on: " + fib(t));
+			}
+			
+			
+		
 
 	}
-
 }
