c8450d8ad7335fa676e1ccaec6d2ff04c67444cf
Tanel <Tanel@EDI>
2013-11-14 12:12:35 +0200

    tfuuyg

--- src/praktikum6/ÄraArvamisMäng.java
+++ src/praktikum6/ÄraArvamisMäng.java
@@ -1,34 +1,49 @@
 package praktikum6;
 
+import lib.TextIO;
+
 public class ÄraArvamisMäng {
+
+	public static void main(String[] args) {
+		TextIO.putln("Mängime veidi, ma valin suvalise arvu");
+	      TextIO.putln("1-st 100-ni, ja sina proovid ära arvata.");
+	      TextIO.put("Paku üks number: ");
+	      boolean mängiUuesti;
+	      do {
+	        Mäng();  // call subroutine to play one game
+	         TextIO.put("Kas soovid veel mängida? Jah (Y)/Ei (N)");
+	         mängiUuesti = TextIO.getlnBoolean();
+	         TextIO.put("Paku üks number: ");
+	      } while (mängiUuesti);
+	      TextIO.putln("Tänan mängimast. Järgmise korrani!.");
+	   } // end of main()            
+		
+	static void Mäng() {
 	
+		int arvuti = SuvaKlass.suvalineArv(1, 100);
+		int arva = 0;
+		while (true) {
 
-
-		public static void main(String[] args) {
-			while (true) {
-				
-				int arvuti = SuvaKlass.suvalineArv(1, 100);
-				
-				System.out
-						.println("Sisesta üks arv (1 - 100) ja näed, kas suudad ära arvata.");
-				while (true) {
-					
-					int kasutajaArv = kasutajaSisestus.kasutaja(1, 100);
-					if (arvuti == kasutajaArv) {
-						System.out.println("Ära arvasid!");
-						break;
-					} else {
-
-						if (arvuti > kasutajaArv) {
-							System.out.println("Pakutud arv on liiga väike. Proovi veel.");
-						} else {
-							System.out.println("Pakutud arv on liiga suur. Proovi veel.");
-						}
-					}
-
+			int kasutajaArv = kasutajaSisestus.kasutaja(1, 100);
+			arva ++;
+			if (arvuti == kasutajaArv) {
+				System.out.println("Ära arvasid! Said " + arva + " korraga hakkama! ");
+				break;
+			}
+			if (arva == 6) {
+			System.out.println("Arvamisvõimalused said otsa. Õige number oli "+ arvuti);
+				break;
+			} else {
+				if (arvuti > kasutajaArv) {
+					System.out
+							.println("Pakutud arv on liiga väike. Sul on " + (6-arva) + " võimalust veel");
+				} else {
+					System.out
+							.println("Pakutud arv on liiga suur. Sul on " + (6-arva) + " võimalust veel");
 				}
 
 			}
 		}
 	}
 
+}
